876fc395bf9640c3d9e4c9099712f22fdc587185
chuckie-xch <1205241831@qq.com>
2017-12-25 23:26:53 +0800

    first commit

---
+++ discovery-euraka/src/main/java/com/bestcode/cloud/study/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.bestcode.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka服务发现
+ *
+ * @author xch
+ * @create 2017-12-24 21:58
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
